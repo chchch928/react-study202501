c5ff171c0ef9bdc9619aa4beb68a7446106fcf3a
chchch928 <kbanda@naver.com>
2025-02-03 12:05:54 +0900

    #0203-04 props 기초

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,23 @@
-import React from 'react';
 import ExpenseItem from "./components/expenses/ExpenseItem.jsx";
 
 const App = () => {
     return (
         <>
-         <ExpenseItem />
+            <ExpenseItem
+                title={'닭강정'}
+                price={8000}
+                date={new Date(2025, 1, 3)}
+            />
+            <ExpenseItem
+                title={'아이스크림'}
+                price={3000}
+                date={new Date(2025, 1, 1)}
+            />
+            <ExpenseItem
+                title={'자바책 구매'}
+                price={30000}
+                date={new Date(2024, 12, 23)}
+            />
         </>
     );
 };
