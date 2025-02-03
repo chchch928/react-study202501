a455b44978839025b20a0b0eb887f4440543848c
chchch928 <kbanda@naver.com>
2025-02-03 12:24:42 +0900

    #0203-05 하위 컴포넌트에게 props 전달하기

--- eslint.config.js
+++ eslint.config.js
@@ -28,7 +28,10 @@ export default [
       ...react.configs.recommended.rules,
       ...react.configs['jsx-runtime'].rules,
       ...reactHooks.configs.recommended.rules,
+      'no-unused-vars': 'off',
+      'react/prop-types':'off',
       'react/jsx-no-target-blank': 'off',
+
       'react-refresh/only-export-components': [
         'warn',
         { allowConstantExport: true },
--- src/components/expenses/ExpenseItem.jsx
+++ src/components/expenses/ExpenseItem.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import './ExpenseItem.css';
+import ExpenseDate from "./ExpenseDate.jsx";
 
 const ExpenseItem = ( { title, price, date }) => {
 
     return (
         <div className='expense-item'>
-            <div>{date.toLocaleString()}</div>
+            <ExpenseDate expenseDate = {date} />
             <div className='expense-item__description'>
                 <h2>{title}</h2>
                 <div className='expense-item__price'>{price}원</div>
