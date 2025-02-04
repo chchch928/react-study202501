25d234e73c0b1c592bad25e186a7d611fd43eb49
chchch928 <kbanda@naver.com>
2025-02-04 12:16:29 +0900

    #0204-08 이전 상태값 반영해서 업데이트 해보기

--- src/App.jsx
+++ src/App.jsx
@@ -28,7 +28,7 @@ const App = () => {
 
     return (
         <>
-            <CheckBoxStyle />
+            <Counter />
         <NewExpense />
         <ExpenseList expenses = {expenses}/>
         </>
