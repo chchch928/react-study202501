5d9f1214f154c0971235d127f84651da94755b9e
chchch928 <kbanda@naver.com>
2025-02-04 10:17:41 +0900

    #0204-02 useState 기초

--- src/components/expenses/ExpenseItem.jsx
+++ src/components/expenses/ExpenseItem.jsx
@@ -1,32 +1,40 @@
-import React from 'react'
+import React, {useState} from 'react'
 import './ExpenseItem.css';
 import ExpenseDate from "./ExpenseDate.jsx";
 import Card from "../ui/Card.jsx";
 import log from "eslint-plugin-react/lib/util/log.js";
 
 const ExpenseItem = ( { expense }) => {
-
     const {title,price,date} = expense;
+  // 상태를 처리하는 함수 (hook)
+  /*
+    useState 훅의 리턴값은 배열이며
+    첫번째 요소는 관리할 상태값의 초기값
+    두번째 요소는 해당 상태값을 변경할때 사용하는 함수 setter 함수
+   */
+  let [itemTitle, setItemTitle] = useState(title);
+  console.log('itemTitle:', itemTitle);
 
-    const formatPrice = new Intl.NumberFormat('ko-KR').format(price);
+  const formatPrice = new Intl.NumberFormat('ko-KR').format(price);
 
     // 이벤트 핸들러 정의
   function handleClick(e) {
-    console.log('click!!');
+    /*
+      리액트는 단순히 변수가 변경된다고 화면의 렌더링을 변경하지 않고
+      오로지 상태값의 변경에 의해서만 렌더링을 변경
+    */
+    setItemTitle ('짜장면');
   }
 
     return (
       <Card className='expense-item'>
         <ExpenseDate expenseDate={date}/>
         <div className='expense-item__description'>
-          <h2>{title}</h2>
+          <h2>{itemTitle}</h2>
           <div className='expense-item__price'>{formatPrice}원</div>
         </div>
-        <button id='btn1' onClick={handleClick}> 버튼1 </button>
-        <button id='btn2'
-                onMouseLeave={e => alert('나가기')}
-                onMouseOver={e => alert('이벤트!!')}>
-          버튼2 </button>
+        <button id='btn1' onClick={handleClick}> 수정 </button>
+
       </Card>
     );
 }
