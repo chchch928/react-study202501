26d5157768d508d1dd69c35aa2f9875480acd71f
chchch928 <kbanda@naver.com>
2025-02-04 10:39:47 +0900

    #0204-04 카운터 로그

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -7,16 +7,20 @@ const Counter = () => {
   // 2 - 상태변경 함수
 
   const [count, setCount] = useState(0);
+  console.log('1. init count: ', count);
 
   const increaseHandler = () => {
     // 상태값을 업데이트 할 때는 상태값에 직접 접근하면 안되고
     // 반드시 setter를 통해 업데이트 해야함
 
     setCount(count+1);
+    // 상태업데이트는 리렌더링 이후에 일어남
+    console.log('2. count by click: ', count);
   }
 
   const decreaseHandler = () => {
     setCount(count-1);
+    console.log('3. last count: ', count);
   }
 
 
