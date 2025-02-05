d3ea15f6b05b8195bc57c7a4d7770f9c2c8918df
chchch928 <kbanda@naver.com>
2025-02-05 09:44:41 +0900

    #0205-02 동적 리스트 렌더링과 key prop

--- src/components/expenses/ExpenseList.jsx
+++ src/components/expenses/ExpenseList.jsx
@@ -1,23 +1,35 @@
-import React from 'react';
+import React, {useState} from 'react';
 import ExpenseItem from './ExpenseItem';
 import './ExpenseList.css';
-import ExpenseFilter from "./ExpenseFilter.jsx";
+import ExpenseFilter from './ExpenseFilter';
 
 const ExpenseList = ({ expenses }) => {
 
-  // 연도를 올리기 위한 함수
+  const [year,setYear] = useState(new Date().getFullYear().toString());
+
+  // 연도를 끌어올리기 위한 함수
   const onFilterChange = (filteredYear) => {
-    console.log(`끌어올려진 연도 :${filteredYear}`)
+
+    console.log(`선택된 끌어올려진 연도: ${filteredYear}`);
+    setYear(filteredYear);
   };
 
-    return (
-        <div className='expenses'>
-            <ExpenseFilter onChangeFilter = {onFilterChange} />
-            <ExpenseItem expense={expenses[0]} />
-            <ExpenseItem expense={expenses[1]} />
-            <ExpenseItem expense={expenses[2]} />
-        </div>
-    );
+  // const createItemArray = () => {
+  //   // expenses라는 객체배열을 ExpenseItem컴포넌트 배열로 변환
+
+  //   return expenses.map(ex => <ExpenseItem expense={ex} />);
+  // };
+
+
+  return (
+    <div className='expenses'>
+      <ExpenseFilter onChangeFilter={onFilterChange} />
+      {expenses
+        .filter(ex => ex.date.getFullYear().toString() === year )
+        // 반복문으로 렌더링할때는 key를 넣어라 리액트 오작동 가능성이 높다
+        .map((ex) => (<ExpenseItem key={Math.random()} expense={ex} />))}
+    </div>
+  );
 };
 
 export default ExpenseList;
