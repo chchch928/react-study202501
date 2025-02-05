17f1d8f1bebfea8a125f87e701b3fe2b3da94efc
chchch928 <kbanda@naver.com>
2025-02-05 10:32:16 +0900

    #0205-04 조건부 렌더링으로 지출항목 렌더링처리

--- src/components/expenses/ExpenseList.jsx
+++ src/components/expenses/ExpenseList.jsx
@@ -1,12 +1,13 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import ExpenseItem from './ExpenseItem';
 import './ExpenseList.css';
 import ExpenseFilter from './ExpenseFilter';
 
 const ExpenseList = ({ expenses }) => {
 
-  const [year,setYear] = useState(new Date().getFullYear().toString());
+  const [year, setYear] = useState(new Date().getFullYear().toString());
 
+  // ExpenseFilter에서 선택한 연도값을 여기서 출력
   // 연도를 끌어올리기 위한 함수
   const onFilterChange = (filteredYear) => {
 
@@ -14,20 +15,35 @@ const ExpenseList = ({ expenses }) => {
     setYear(filteredYear);
   };
 
+
+
   // const createItemArray = () => {
   //   // expenses라는 객체배열을 ExpenseItem컴포넌트 배열로 변환
 
   //   return expenses.map(ex => <ExpenseItem expense={ex} />);
   // };
 
+  // 지출 데이터가 없을 때 보여줄 컴포넌트
+  let content = <p>아직 해당년도의 지출항목이 없습니다.</p>;
+
+  // 지출 데이터가 있을 때 보여줄 컴포넌트
+  const filteredExpenses = expenses
+    .filter((ex) => ex.date.getFullYear().toString() === year);
+
+  if (filteredExpenses.length > 0) {
+    content = filteredExpenses.map((ex) => (
+      <ExpenseItem
+        key={Math.random()}
+        expense={ex}
+      />
+    ));
+  }
+
 
   return (
     <div className='expenses'>
       <ExpenseFilter onChangeFilter={onFilterChange} />
-      {expenses
-        .filter(ex => ex.date.getFullYear().toString() === year )
-        // 반복문으로 렌더링할때는 key를 넣어라 리액트 오작동 가능성이 높다
-        .map((ex) => (<ExpenseItem key={Math.random()} expense={ex} />))}
+      { content }
     </div>
   );
 };
