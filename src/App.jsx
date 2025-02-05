a5d6542e79c0cf55d21cba18ba8f5a5fbd737c42
chchch928 <kbanda@naver.com>
2025-02-05 12:33:59 +0900

    #0205-08 종합복습1-2 - 목표설정 앱 템플릿

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,27 @@
 import './App.css'
 import CourseInput from "./components/CourseGoals/CourseInput.jsx";
 import CourseList from "./components/CourseGoals/CourseList.jsx";
+import {useState} from "react";
 
 // 컴포넌트
 const App = () => {
 
+    // 목표 데이터들의 묶음배열
+    const [goals, setGoals] = useState([]);
 
-
-
-
+    // CourseInput에게 전달할 함수
+    const onAddGoal = (goal) => {
+        setGoals([...goals,goal]);
+    };
 
     return (
       <div>
           <section id = 'goal-form'>
-              <CourseInput />
+              <CourseInput onAdd = {onAddGoal} />
           </section>
 
           <section id = 'goals'>
-              <CourseList />
+              <CourseList items={goals} />
           </section>
       </div>
     );
